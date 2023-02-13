8016679bfe3b9b7356930c4fe7fc7a92ead7286d
julie_c <forechecker@gmail.com>
2023-02-13 14:00:59 -0700

    Added event listener and displayed images

--- js/script.js
+++ js/script.js
@@ -6,7 +6,30 @@ const img = document.querySelector(".img");
 const getImage = async function () { 
     const response = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await response.json();
-    console.log(images);
+    //console.log(images);
+    selectRandomImage(images);
 }
 
-getImage();
+const selectRandomImage = function (images) { 
+    let randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    let randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+//This function will be accepting the 
+//random image object, which contains all the properties listed above.
+const displayImage = function (randomImage) { 
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
+
